010e68fd95b16668d6f670b48cfbaa1dcf76d878
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2021-04-03 01:49:40 -0600

    Switch to SQLAlchemy and begin to add API endpoints
    * Add endpoint to get all employees.
    * Add endpoint to add employees.
    * Remove dead insecure code.

--- src/SimpleServer.py
+++ src/SimpleServer.py
@@ -1,89 +1,109 @@
-## CLOCKWISE-BOOTCAMP SimpleServer.py 
-## Based on Server from Dr. Ian Cooper @ Cardiff
-## Updated by Dr. Mike Borowczak @ UWyo March 2021
+# CLOCKWISE-BOOTCAMP SimpleServer.py
+# Based on Server from Dr. Ian Cooper @ Cardiff
+# Updated by Dr. Mike Borowczak @ UWyo March 2021
 
-import os
-from flask import Flask, redirect, request, render_template
+import secrets
 import sqlite3
 
-DATABASE = 'bootcamp.db'
+from flask import Flask, render_template, request
+
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bootcamp.db"
+
+# Generate a safe secret key on the fly
+app.secret_key = secrets.token_urlsafe(32)
+
+# Load the database
+db = SQLAlchemy(app)
+
+
+class Employee(db.Model):
+    """Database Model"""
+
+    __tablename__ = "EmployeeList"
+    __table_args = {"extend_existing": True}
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column("firstName", db.Text)
+    last_name = db.Column("lastName", db.Text)
+    hours = db.Column("FT/PT/.75?", db.Text)
+    business_unit = db.Column("Business Unit", db.Text)
+    city = db.Column("City", db.Text)
+    state = db.Column("State/Province", db.Text)
+    career_matrix = db.Column("Career Matrix Title", db.Text)
+    total_years = db.Column("Total years", db.Text)
+    licenses = db.Column("Registered Licenses", db.Text)
+    skill = db.Column("Skill", db.Text)
+    skill_level = db.Column("Skill Level", db.Text)
+
 
 @app.route("/")
-def basic():
-    return render_template('Employee.html')
+def root():
+    """Dump all the employees to a table."""
+    # TODO: Make this dynamically load
+    # TODO: Make this prettier
+    data = Employee.query.all()
+    return render_template("Employee.html", data=data)
 
-@app.route("/Employee/AddEmployee", methods = ['POST','GET'])
+
+@app.route("/Employee/AddEmployee", methods=["GET", "POST"])
 def studentAddDetails():
-	if request.method =='GET':
-		return render_template('EmployeeData.html')
-	if request.method =='POST':
-		firstName = request.form.get('firstName', default="Error") 
-		lastName = request.form.get('lastName', default="Error")
-		businessunit = request.form.get('bu', default="Error")
-		state = request.form.get('state', default="Error")
-		print("inserting employee"+firstName)
-		try:
-			conn = sqlite3.connect(DATABASE)
-			cur = conn.cursor()
-			cur.execute("INSERT INTO EmployeeList ('FirstName', 'LastName', 'Business Unit', 'State/Province')\
-						VALUES (?,?,?,?)",(firstName, lastName, businessunit, state ) )
+    if request.method == "GET":
+        return render_template("EmployeeData.html")
 
-			conn.commit()
-			msg = "Record successfully added"
-		except:
-			conn.rollback()
-			msg = "error in insert operation"
-		finally:
-			conn.close()
-			return msg
+    # If a field is left empty do nothing
+    # TODO: Implement a pretty way of telling the user
+    if "" in request.form.values():
+        return ("", 204)
 
-@app.route("/Employee/Search", methods = ['GET','POST'])
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+
+    # Check if the employee already exists
+    employee = Employee.query.filter_by(
+        first_name=first_name, last_name=last_name
+    ).first()
+
+    # Update if the employee exists
+    # TODO: Show some pretty confirmation
+    if employee is not None:
+        employee.first_name = request.form.get("first_name")
+        employee.last_name = request.form.get("last_name")
+        employee.business_unit = request.form.get("business_unit")
+        employee.state = request.form.get("state")
+    else:
+        employee = Employee(**request.form)
+        print(employee.first_name)
+        db.session.add(employee)
+
+    db.session.commit()
+    return render_template("EmployeeData.html")
+
+
+@app.route("/Employee/Search", methods=["GET", "POST"])
 def surnameSearch():
-	if request.method =='GET':
-		return render_template('EmployeeSearch.html')
-	if request.method =='POST':
-		try:
-			lastName = request.form.get('lastName', default="Error") #rem: args for get form for post
-			conn = sqlite3.connect(DATABASE)
-			cur = conn.cursor()
-			cur.execute("SELECT * FROM 'EmployeeList' WHERE LastName=?", [lastName])
-			data = cur.fetchall()
-			print(data)
-		except:
-			print('there was an error', data)
-			conn.close()
-		finally:
-			conn.close()
-			#return str(data)
-			return render_template('Employee.html', data = data)
-
-
-
-# The name says it...
-@app.route("/Employee/VulnerableSearch", methods = ['GET','POST'])
-def surnameInjectionSearch():
-	if request.method =='GET':
-		return render_template('EmployeeSQLInjection.html')
-	if request.method =='POST':
-		lastName = request.form.get('lastName', default="Error") #rem: args for get form for post
-		conn = sqlite3.connect(DATABASE)
-		cur = conn.cursor()
-		
-		# VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD
-		query = "SELECT * FROM EmployeeList WHERE lastname= '%s' " % (lastName,)
-		print (query)
-		cur.execute(query)
-		# VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD
-		
-		data = cur.fetchall()
-		print (data)
-		print (lastName)
-		conn.close()
-		return render_template('Employee.html', data = data)
+    if request.method == "GET":
+        return render_template("EmployeeSearch.html")
+    try:
+        lastName = request.form.get(
+            "lastName", default="Error"
+        )  # rem: args for get form for post
+        conn = sqlite3.connect(DATABASE)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM 'EmployeeList' WHERE LastName=?", [lastName])
+        data = cur.fetchall()
+        print(data)
+    except Exception as e:
+        print("there was an error", e, data)
+        conn.close()
+    finally:
+        conn.close()
+        # return str(data)
+        return render_template("Employee.html", data=data)
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
-	app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
